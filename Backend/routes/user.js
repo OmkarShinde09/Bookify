36c093ca663b48abb2002f762f59db13ee3b86b0
OmkarShinde09 <omkarshine07091969@gmail.com>
2024-04-21 09:37:42 +0530

    Fixed signin

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -12,30 +12,25 @@ router.get("/signup", (req, res) => {
 });
 
 router.post("/signin", async (req, res) => {
-    const { email, password } = req.body;
-    try {
-      const token = await User.matchPasswordAndGenerateToken(email, password);
-  
-      return res.cookie("token", token).redirect("/");
-    } catch (error) {
-      return res.render("signin", {
-        error: "Incorrect Email or Password",
-      });
-    }
-  });
-  
-router.get("/logout", (req, res) => {
-res.clearCookie("token").redirect("/");
+  const { email, password } = req.body;
+  const user = await User.matchPassword(email, password);
+
+  console.log('User', user);
+  return res.redirect("/");
 });
-  
+
+router.get("/logout", (req, res) => {
+  res.clearCookie("token").redirect("/");
+});
+
 router.post("/signup", async (req, res) => {
-const { fullName, email, password } = req.body;
-await User.create({
+  const { fullName, email, password } = req.body;
+  await User.create({
     fullName,
     email,
     password,
+  });
+  return res.redirect("/");
 });
-return res.redirect("/");
-});
-  
+
 module.exports = router;
